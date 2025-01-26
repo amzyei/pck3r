585b9012eed54db2ac043e9754866af342dd2b3c
amzyei6@gmail.com <amzyei6@gmail.com>
2025-01-26 14:12:00 +0330

    installer method changed!

--- installer.py
+++ installer.py
@@ -19,13 +19,11 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 from libs import stuff
 from os import  getcwd, getenv, chdir
-from os import system as syscall
+from os import system 
 ###############################################################################
 # preinstall requirements                                                     #
-syscall('mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon')#
-syscall('sudo apt install python3-tk -y')                                     #
-syscall('sudo apt install python3-pil python3-pil.imagetk -y')                #
-syscall('sudo apt install g++ -y')                                            # 
+system('mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon')#
+system('sudo apt install python3-tk python3-pil python3-pil.imagetk  g++ -y')                                            # 
 ###############################################################################
 import tkinter as tk
 from tkinter.ttk import *
@@ -36,7 +34,7 @@ from PIL import ImageTk,Image
 root = tk.Tk()
 
 
-make_link = 'sudo ln -s %s/.pck3r/core_pck3r.py /bin/pck3r' % getenv('HOME')
+make_link = 'sudo ln -s %s/.pck3r/main.py /bin/pck3r' % getenv('HOME')
 
 # Progress bar widget
 progress = Progressbar(root, length = 100, mode = 'determinate')
@@ -50,31 +48,31 @@ def bar():
     root.update_idletasks()
     time.sleep(1)
     print('Unlinking pck3r (if was installed) ')
-    syscall('sudo unlink /bin/pck3r ')
-    syscall('sudo rm -rf /bin/pck3r*')
+    system('sudo unlink /bin/pck3r ')
+    system('sudo rm -rf /bin/pck3r*')
 
     # 40% PROGRESS
     progress['value'] = 40
     root.update_idletasks()
     time.sleep(1)
    
-    syscall('rm -rf ~/.pck3r && sudo rm -rf /root/.pck3r') if (syscall('echo %s ; ls ~/.pck3r' % stuff.CYN)) ==0 else print('%sCopy all pck3r directory %s' %(stuff.CYN, stuff.NRM))
-    syscall('mkdir ~/.pck3r')
-    syscall('cp -rf . ~/.pck3r')
-    syscall('sudo cp -rf . /root/.pck3r')
+    system('rm -rf ~/.pck3r && sudo rm -rf /root/.pck3r') if (system('echo %s ; ls ~/.pck3r' % stuff.CYN)) ==0 else print('%sCopy all pck3r directory %s' %(stuff.CYN, stuff.NRM))
+    system('mkdir ~/.pck3r')
+    system('cp -rf . ~/.pck3r')
+    system('sudo cp -rf . /root/.pck3r')
 
     # 50% PROGRESS
     progress['value'] = 50
     root.update_idletasks()
     time.sleep(1)
-    syscall(make_link)
+    system(make_link)
 
     # 60% PROGRESS
     progress['value'] = 60
     root.update_idletasks()
     time.sleep(1)
     print('%sCheck link ' % stuff.YEL)
-    print('Link created ') if (syscall('ls -l  /bin/pck3r')) == 0  else print('%s%sNo link (/bin/pck3r)%s ' % (stuff.sysOk(), stuff.RED, stuff.NRM))
+    print('Link created ') if (system('ls -l  /bin/pck3r')) == 0  else print('%s%sNo link (/bin/pck3r)%s ' % (stuff.sysOk(), stuff.RED, stuff.NRM))
     progress['value'] = 80
     root.update_idletasks()
     time.sleep(1)
