a3f1cb23a9212510fd7789d3b2e278944a3d7b27
amzyei6@gmail.com <amzyei6@gmail.com>
2025-01-26 14:45:59 +0330

    help updated(scripts deleted)

--- libs/help.py
+++ libs/help.py
@@ -18,114 +18,85 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from os import system as syscall
-from os import chdir, getcwd, getenv
+from os import getenv
 
-if __name__ == "__main__":
-    print("""
-This is a module not an executeable program
-Alternative command :
-$ python3 core_pck3r.py
-OR
-$ python3 installer.py
-OR
-$ chmod 755 core_pck3r.py ; ./core_pck3r.py
-And for installing :
-$ chmod 755 installer.py ; ./installer.py
-    """)
+from . import stuff
 
-else:
-    from . import stuff
-
-    message ="""%s
-    -----------------------------------------
-    |                                       |
-    | pck3r : It is a versatile program and |
-    |                                       |
-    | you avoid using useless commands and  |
-    |                                       |
-    | it is written for Ubuntu...           |
-    |                                       |
-    -----------------------------------------
+message ="""%s
+-----------------------------------------
+|                                       |
+| pck3r : It is a versatile program and |
+|                                       |
+| you avoid using useless commands and  |
+|                                       |
+| it is written for Ubuntu...           |
+|                                       |
+-----------------------------------------
 
 
 \"install\" command :
 
-    $ pck3r install \"somthing\" :
-    {
-        nodejs,
-        wine,
-        ohmyzsh,
-        flstudio,
-        minecraft,
-        or ...
-    }
+$ pck3r install \"somthing\" :
+{
+    nodejs,
+    wine,
+    ohmyzsh,
+    flstudio,
+    minecraft,
+    or ...
+}
 
 \"clear\" command :
 
-    $ pck3r clear:
-    {clear your terminal }
+$ pck3r clear:
+{clear your terminal }
 
 
 \"sys\" command :
 
-    $ pck3r sys update
-    (update your oprating system)
+$ pck3r sys update
+(update your oprating system)
 
-    $ pck3r sys upgrade
-    (upgrade your oprating system)
+$ pck3r sys upgrade
+(upgrade your oprating system)
 
-    $ pck3r sys updgr
-    (both, update and upgrade (full upgrade))
+$ pck3r sys updgr
+(both, update and upgrade (full upgrade))
 
 
 \"tilix\" command :
 
-    $ pck3r tilix
-    (tilix terminal ...)
+$ pck3r tilix
+(tilix terminal ...)
 
 \"dotnet\" command :
 
-    $ pck3r install dotnet
-    (installing .NET (dot net ) C0RE, ASP, MCS compiler , ...)
+$ pck3r install dotnet
+(installing .NET (dot net ) C0RE, ASP, MCS compiler , ...)
 
 \"pkg\" command :
 
-    $ pck3r pkg <package name>
-    (search for packages ...)
+$ pck3r pkg <package name>
+(search for packages ...)
 
 \"update\" command :
 
-    $ pck3r update
-    (update to last release from github.com/amzy-0/pck3r)
-    
+$ pck3r update
+(update to last release from github.com/amzy-0/pck3r)
+
 \"minecraft\" command :
 
-    $ pck3r minecraft
-    (minecraft runner)
+$ pck3r minecraft
+(minecraft runner)
 
 "version" command :
 
-    $ pck3r version
-    (this command show pck3r version)
+$ pck3r version
+(this command show pck3r version)
 
 
-            %s
-    """ % (stuff.YEL, stuff.NRM)
+        %s
+""" % (stuff.YEL, stuff.NRM)
 
 
 
-
-    # pck3r heart runner
-    
-    if (syscall(' %s/.pck3r/scripts/./pck3r-heart.rb' %getenv('HOME')))==0:
-        print(message)
-        syscall('~/.pck3r/scripts/./pck3r-heart.rb')
-
-    # if ruby not installed 
-    else:
-        print('%sdependency detected : ruby-full%s' % (stuff.YEL, stuff.NRM))
-        
-        syscall('sudo apt install ruby-full')
-        syscall('clear')
-        print(message)
-        syscall('~/.pck3r/scripts/./pck3r-heart.rb')
