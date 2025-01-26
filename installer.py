0829d86cf35f76993b3e081619e32f123a8c19e3
amzyei6@gmail.com <amzyei6@gmail.com>
2025-01-26 14:25:31 +0330

    installer's method changed

--- installer.py
+++ installer.py
@@ -18,86 +18,43 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 from libs import stuff
-from os import  getcwd, getenv, chdir
+from os import  getenv
 from os import system 
-###############################################################################
-# preinstall requirements                                                     #
-system('mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon')#
-system('sudo apt install python3-tk python3-pil python3-pil.imagetk  g++ -y')                                            # 
-###############################################################################
-import tkinter as tk
-from tkinter.ttk import *
 import time
-from PIL import ImageTk,Image
+###############################################################################
+# Preinstall requirements                                                     #
+system('pip install -r requirements.txt --bre')
+###############################################################################
+# Copy icon to the PATH
+system('mkdir -p ~/.pck3r/icon ; cp -rf ./icon/pck3r-logo.png ~/.pck3r/icon')
 
 # creating tkinter window
-root = tk.Tk()
 
 
 make_link = 'sudo ln -s %s/.pck3r/main.py /bin/pck3r' % getenv('HOME')
 
 # Progress bar widget
-progress = Progressbar(root, length = 100, mode = 'determinate')
 
 # Function responsible for the updation
 # of the progress bar value
-def bar():
+def install():
 
-    # 20% PROGRESS
-    progress['value'] = 20
-    root.update_idletasks()
     time.sleep(1)
     print('Unlinking pck3r (if was installed) ')
     system('sudo unlink /bin/pck3r ')
     system('sudo rm -rf /bin/pck3r*')
-
-    # 40% PROGRESS
-    progress['value'] = 40
-    root.update_idletasks()
-    time.sleep(1)
    
     system('rm -rf ~/.pck3r && sudo rm -rf /root/.pck3r') if (system('echo %s ; ls ~/.pck3r' % stuff.CYN)) ==0 else print('%sCopy all pck3r directory %s' %(stuff.CYN, stuff.NRM))
     system('mkdir ~/.pck3r')
     system('cp -rf . ~/.pck3r')
     system('sudo cp -rf . /root/.pck3r')
-
-    # 50% PROGRESS
-    progress['value'] = 50
-    root.update_idletasks()
-    time.sleep(1)
     system(make_link)
-
-    # 60% PROGRESS
-    progress['value'] = 60
-    root.update_idletasks()
     time.sleep(1)
     print('%sCheck link ' % stuff.YEL)
     print('Link created ') if (system('ls -l  /bin/pck3r')) == 0  else print('%s%sNo link (/bin/pck3r)%s ' % (stuff.sysOk(), stuff.RED, stuff.NRM))
-    progress['value'] = 80
-    root.update_idletasks()
     time.sleep(1)
-
-    # 100% PROGRESS
-    progress['value'] = 100
     print('%s%sPck3r installed successfuly %s' % (stuff.sysOk(), stuff.GRN, stuff.NRM))
     
-    root.quit()
 
 
-# packing to main window (panel)
-icon = Image.open('%s/.pck3r/icon/pck3r-logo.png' % getenv('HOME'))
-root.title('Pck3r Installer')
-photo = ImageTk.PhotoImage(icon)
-root.wm_iconphoto(False, photo)
-root.geometry('400x80')
-root.configure(background='black')
-root.resizable(False, False)
-progress.pack(fill='x', pady = 10)
-installBtn = tk.Button(root, text='install pck3r (system wide) ', command = bar)
-installBtn.configure(fg='lightblue', bg='darkblue', )
-
-installBtn.pack(fill='x', pady = 10)
-
-# infinite loop
-tk.mainloop()
-
+install()
