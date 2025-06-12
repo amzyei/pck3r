8d2936104f280c6bc4618105bad9e1958a2a10f5
amzyei <azymiamin@gmail.com>
2025-06-12 13:30:37 -0700

    FIX: cls command

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]) {
     }
 
     std::map<std::string, bool> validCommands = {
-        {"clear", true},
+        {"cls", true},
         {"install", true},
         {"version", true}
     };
