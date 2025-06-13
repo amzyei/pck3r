3a2e79a360a95cae8025764752efcd4a27b1f2f3
amzyei <azymiamin@gmail.com>
2025-06-12 20:03:43 -0700

    #include nodejs.h

--- include/install.h
+++ include/install.h
@@ -4,7 +4,7 @@
 #include "stuff.h"
 #include "chrome.h"
 #include "firefox.h"
-
+#include "nodejs.h"
 void installNodejs();
 void installFirefox();
 void handleGenericInstall(const std::string& packageName);
@@ -35,42 +35,5 @@ inline void handleGenericInstall(const std::string& packageName) {
 #endif
 }
 
-inline void installNodejs() {
-#ifdef _WIN32
-    std::cout << WAIT_FOR_PROCESSING << std::endl;
-    const std::string url = "https://nodejs.org/dist/v22.16.0/node-v22.16.0-x64.msi";
-    const std::string installer = "node-v22.16.0-x64.msi";
-
-    // PowerShell command to download the installer
-    std::string downloadCmd = "powershell -Command \"Invoke-WebRequest -Uri '" + url + "' -OutFile '" + installer + "'\"";
-
-    // Run the download command
-    int ret = system(downloadCmd.c_str());
-    if (ret != 0) {
-        std::cout << "Failed to download Node.js installer." << std::endl;
-        return;
-    }
-
-    // Run the installer asynchronously using start command to avoid freezing
-    std::string installCmd = "start " + installer;
-
-    ret = system(installCmd.c_str());
-    if (ret == 0) {
-        std::cout << sysOk("Node.js installer started successfully!") << std::endl;
-    } else {
-        std::cout << sysErr("") << "Failed to start Node.js installer." << std::endl;
-    }
-#else
-    std::cout << sysErr("") << "Node.js installation is not supported on this platform." << std::endl;
-#endif
-}
-
-inline void installOhMyZsh() {
-#ifdef _WIN32
-    std::cout << sysErr("") << "Oh My Zsh installation is not supported on Windows." << std::endl;
-#else
-    std::cout << sysErr("") << "Oh My Zsh installation is not supported on this platform." << std::endl;
-#endif
-}
 
 #endif // INSTALL_H
