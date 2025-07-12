b8a82ba67dcc9d0881e51f549f3f50feff2298fa
amzyei <azymiamin@gmail.com>
2025-07-12 07:45:58 +0330

    Stage changes in libs/firefox.py and libs/wine.py

--- libs/wine.py
+++ libs/wine.py
@@ -30,7 +30,7 @@ def wine_installer():
     sudo dpkg --add-architecture i386; 
     wget -nc https://dl.winehq.org/wine-builds/winehq.key;
     sudo apt-key add winehq.key;
-    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ noble main' ;
+    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ jammy main' ;
     sudo apt install --install-recommends winehq-stable;
     """))!=0:
 
